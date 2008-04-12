ea25359edf4d968954f40d401e9adbc5d52aece0
nemerle <nemerle@a19be42f-918d-4297-8077-ab25d1e4f51f>
2008-04-12 12:59:59 +0000

    Simple Connector implemetation... no configuration .. should transform it into generic client component
    git-svn-id: svn://svn.code.sf.net/p/segs/code2@44 a19be42f-918d-4297-8077-ab25d1e4f51f

--- client/examples/src/generated/LoginDialog.cpp
+++ client/examples/src/generated/LoginDialog.cpp
@@ -3,10 +3,122 @@
 #include "PacketBase.h"
 #include "LoginDialog.h"
 #include "Avatar.h"
+#include <ace/Connector.h>
+#include <ace/Svc_Handler.h>
+#include <ace/SOCK_Connector.h>
+#include <ace/SOCK_Stream.h>
+#include <ace/Reactor_Notification_Strategy.h>
+
+
 // begin wxGlade: ::extracode
 
-// end wxGlade
 
+// end wxGlade
+class Auth_Handler : public ACE_Svc_Handler<ACE_SOCK_STREAM,ACE_NULL_SYNCH>
+{
+    typedef ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_NULL_SYNCH> PARENT;
+
+    typedef ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_NULL_SYNCH> super;
+
+public:
+    Auth_Handler():notifier_ (0, this, ACE_Event_Handler::WRITE_MASK)
+    {}
+
+    virtual int open (void * = 0);
+
+    // Called when input is available from the client.
+    virtual int handle_input (ACE_HANDLE fd = ACE_INVALID_HANDLE);
+
+    // Called when output is possible.
+    virtual int handle_output (ACE_HANDLE fd = ACE_INVALID_HANDLE);
+
+    // Called when a timer expires.
+    virtual int handle_timeout (const ACE_Time_Value &current_time,
+        const void *act = 0);
+
+private:
+    enum { ITERATIONS = 5 };
+    int iterations_;
+    ACE_Reactor_Notification_Strategy notifier_;
+};
+
+int Auth_Handler::open (void *p)
+{
+    ACE_Time_Value iter_delay (2);   // Two seconds
+    if (super::open (p) == -1)
+        return -1;
+    this->notifier_.reactor (this->reactor ());
+    this->msg_queue ()->notification_strategy (&this->notifier_);
+    return this->reactor ()->schedule_timer(this, 0, ACE_Time_Value::zero, iter_delay);
+}
+int Auth_Handler::handle_input (ACE_HANDLE)
+{
+    char buf[64];
+    ssize_t recv_cnt = this->peer ().recv (buf, sizeof (buf) - 1);
+    if (recv_cnt > 0)
+    {
+        ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("%*C"),ACE_static_cast (int, recv_cnt),buf));
+        return 0;
+    }
+    if (recv_cnt == 0 || ACE_OS::last_error () != EWOULDBLOCK)
+    {
+        this->reactor ()->end_reactor_event_loop ();
+        return -1;
+    }
+    return 0;
+}
+int Auth_Handler::handle_timeout(const ACE_Time_Value &, const void *)
+{
+    if (this->iterations_ >= ITERATIONS)
+    {
+        this->peer ().close_writer ();
+        return 0;
+    }
+
+    ACE_Message_Block *mb;
+    char msg[128];
+    ACE_OS::sprintf (msg, "Iteration %d\n", this->iterations_);
+    ACE_NEW_RETURN (mb, ACE_Message_Block (msg), -1);
+    this->putq (mb);
+    return 0;
+}
+int Auth_Handler::handle_output (ACE_HANDLE)
+{
+    ACE_Message_Block *mb;
+    ACE_Time_Value nowait (ACE_OS::gettimeofday ());
+    while (-1 != this->getq (mb, &nowait))
+    {
+        ssize_t send_cnt = this->peer ().send (mb->rd_ptr (), mb->length ());
+        if (send_cnt == -1)
+            ACE_ERROR ((LM_ERROR,ACE_TEXT ("(%P|%t) %p\n"),ACE_TEXT ("send")));
+        else
+            mb->rd_ptr (ACE_static_cast (size_t, send_cnt));
+        if (mb->length () > 0)
+        {
+            this->ungetq (mb);
+            break;
+        }
+        mb->release ();
+    }
+    if (this->msg_queue ()->is_empty ())
+        this->reactor ()->cancel_wakeup(this, ACE_Event_Handler::WRITE_MASK);
+    else
+        this->reactor ()->schedule_wakeup(this, ACE_Event_Handler::WRITE_MASK);
+    return 0;
+}
+
+
+
+class Auth_Client_Connector : public ACE_Connector<Auth_Handler,ACE_SOCK_CONNECTOR>
+{
+    typedef ACE_Connector<Auth_Handler,ACE_SOCK_CONNECTOR> PARENT;
+public:
+    // Initialization
+    Auth_Client_Connector (void){};
+    ~Auth_Client_Connector (void){};
+private:
+    ACE_INET_Addr remote_addr_; // this is where the auth server lives
+};
 
 LoginDialog::LoginDialog(wxWindow* parent, int id, const wxString& title, const wxPoint& pos, const wxSize& size, long style):
     wxDialog(parent, id, title, pos, size, wxDEFAULT_DIALOG_STYLE)
@@ -17,7 +129,9 @@ LoginDialog::LoginDialog(wxWindow* parent, int id, const wxString& title, const 
     m_server_response = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
     m_btn_login = new wxButton(this, wxID_ANY, wxT("Login"));
     m_btn_cancel = new wxButton(this, wxID_CANCEL, wxEmptyString);
-
+    Auth_Client_Connector cntr;
+    Auth_Handler *hndlr=0;
+    cntr.connect(hndlr,ACE_INET_Addr(2167,"127.0.0.1"));
     set_properties();
     do_layout();
     // end wxGlade
