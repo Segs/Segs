c38252152baa138c66d39bd425a43767fc51d519
nemerle <nemerle@a19be42f-918d-4297-8077-ab25d1e4f51f>
2008-04-15 18:54:19 +0000

    Another big move
    git-svn-id: svn://svn.code.sf.net/p/segs/code2@56 a19be42f-918d-4297-8077-ab25d1e4f51f

--- CMakeScripts/SEGS_Components.cmake
+++ CMakeScripts/SEGS_Components.cmake
@@ -3,21 +3,17 @@ MACRO(SEGS_ADD_COMPONENT component_name LINK_TYPE)
 
 		IF("${LINK_TYPE}" STREQUAL "LIB")
 
-			INCLUDE_DIRECTORIES(${component_name}_INCLUDE_DIR)		
+            SET(target_INCLUDE_DIR ${target_INCLUDE_DIR} ${${component_name}_INCLUDE_DIR})
 			SET(target_DEPENDS ${target_DEPENDS} ${component_name})
-			#MESSAGE(ERROR "Depends On ${target_DEPENDS}")
 
 		ELSEIF("${LINK_TYPE}" STREQUAL "SOURCE")
-            SET(testval "axxxx")
+
             SET(target_INCLUDE_DIR ${target_INCLUDE_DIR} ${${component_name}_INCLUDE_DIR})
-#			SET(target_SOURCES ${target_SOURCES} ${${component_name}_SOURCES})
 			SET(target_CPP ${target_CPP} ${${component_name}_CPP})
 			SET(target_INCLUDE ${target_INCLUDE} ${${component_name}_INCLUDE})
 			SOURCE_GROUP(${component_name}\\Source FILES ${${component_name}_CPP})
 			SOURCE_GROUP(${component_name}\\Headers FILES ${${component_name}_INCLUDE})
-#			MESSAGE(WARNING " Sources when adding ${component_name}:${target_SOURCES}")
-#           MESSAGE(WARNING " Component includes ${component_name}:${${component_name}_INCLUDE_DIR}")
-#           MESSAGE(WARNING " Includes when adding ${component_name}:${target_INCLUDE_DIR}")
+
 		ELSE("${LINK_TYPE}" STREQUAL "LIB")
 
 			MESSAGE(ERROR " Unknown component linking mode ${LINK_TYPE}")
@@ -34,12 +30,16 @@ ENDMACRO(SEGS_ADD_COMPONENT)
 
 MACRO(SEGS_REGISTER_COMPONENT name include_dir sources includes )
 
-	SET(${name}_VISITED TRUE CACHE INTERNAL "")
-	SET(${name}_INCLUDE_DIR ${include_dir} CACHE INTERNAL "")
-#   MESSAGE(WARNING " Setting Component includes ${name}:${include_dir}")
+    IF(${${name}_VISITED})
+		MESSAGE(ERROR " Component ${component_name} already registered.")
+    ELSE(${${name}_VISITED})
+
+        SET(${name}_VISITED TRUE CACHE INTERNAL "")
+        SET(${name}_INCLUDE_DIR ${include_dir} CACHE INTERNAL "")
     
-	SET(${name}_CPP ${sources} CACHE INTERNAL "")	
-	SET(${name}_INCLUDE ${includes} CACHE INTERNAL "")
-    SET(${name}_SOURCES ${${name}_CPP} ${${name}_INCLUDE} CACHE INTERNAL "")
+        SET(${name}_CPP ${sources} CACHE INTERNAL "")	
+        SET(${name}_INCLUDE ${includes} CACHE INTERNAL "")
+        SET(${name}_SOURCES ${${name}_CPP} ${${name}_INCLUDE} CACHE INTERNAL "")
+    ENDIF(${${name}_VISITED})
     
 ENDMACRO(SEGS_REGISTER_COMPONENT)
