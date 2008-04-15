04f91015f105bc8c30599ffc382ec8c1f0abf229
nemerle <nemerle@a19be42f-918d-4297-8077-ab25d1e4f51f>
2008-04-15 20:42:56 +0000

    Final removal. Created Example Project directory structure, and moved pseudo client with wxWidgets Interface, there
    git-svn-id: svn://svn.code.sf.net/p/segs/code2@60 a19be42f-918d-4297-8077-ab25d1e4f51f

--- client/common/include/ClientSession.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// This file is basically a placeholder, until we have better design for client side session management
-class IClientSession
-{
-public:
-	virtual void connect()=0;
-	virtual void disconnect()=0;
-};
