c5c21e03c92ea88bf6ef14217d23919167bbe5b5
nemerle <nemerle@a19be42f-918d-4297-8077-ab25d1e4f51f>
2009-10-24 23:41:15 +0000

    Placeholder files for Hash indexed communications framework It's likely it will be moved to common components.
    git-svn-id: svn://svn.code.sf.net/p/segs/code2@152 a19be42f-918d-4297-8077-ab25d1e4f51f

---
+++ Projects/CoX/Servers/GameServer/include/HashStorage.h
@@ -0,0 +1,11 @@
+/*
+* Super Entity Game Server Project
+* http://segs.sf.net/
+* Copyright (c) 2009 Super Entity Game Server Team (see Authors.txt)
+* This software is licensed! (See License.txt for details)
+*
+* $Id:$
+*/
+#pragma once
+#include <string>
+#include "types.h"
---
+++ Projects/CoX/Servers/GameServer/src/HashStorage.cpp
@@ -0,0 +1,18 @@
+/*
+* Super Entity Game Server Project
+* http://segs.sf.net/
+* Copyright (c) 2009 Super Entity Game Server Team (see Authors.txt)
+* This software is licensed! (See License.txt for details)
+*
+* $Id:$
+*/
+#include <ace/OS.h>
+#include "HashStorage.h"
+
+// SpecHash<std::string,val>
+// 
+// get hash index of given key
+u32 get_hash_idx(const std::string &key)
+{
+	return 0;
+}
