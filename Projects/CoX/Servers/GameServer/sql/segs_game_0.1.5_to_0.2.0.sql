cb930b34bd48f7d5dcf50218d92431ef84684c3a
nemerle <nemerle@a19be42f-918d-4297-8077-ab25d1e4f51f>
2009-11-13 17:39:15 +0000

    Costume storage db schema update. Also added db schema update script
    git-svn-id: svn://svn.code.sf.net/p/segs/code2@164 a19be42f-918d-4297-8077-ab25d1e4f51f

---
+++ Projects/CoX/Servers/GameServer/sql/segs_game_0.1.5_to_0.2.0.sql
@@ -0,0 +1,8 @@
+ALTER TABLE costume_part ADD COLUMN name_0_idx integer NOT NULL;
+ALTER TABLE costume_part ADD COLUMN name_1_idx integer NOT NULL;
+ALTER TABLE costume_part ADD COLUMN name_2_idx integer NOT NULL;
+ALTER TABLE costume_part ADD COLUMN name_3_idx integer NOT NULL;
+ALTER TABLE costume_part ADD COLUMN color_0_idx integer NOT NULL;
+ALTER TABLE costume_part ADD COLUMN color_1_idx integer NOT NULL;
+ALTER TABLE costume_part ADD COLUMN color_2_idx integer NOT NULL;
+ALTER TABLE costume_part ADD COLUMN color_3_idx integer NOT NULL;
