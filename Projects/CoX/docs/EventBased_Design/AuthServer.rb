6d46b447a99b4d7e5554326717eae6c75ba5e89a
nemerle <nemerle@a19be42f-918d-4297-8077-ab25d1e4f51f>
2009-11-15 01:38:07 +0000

    high-level AuthServer is almost finished.
    git-svn-id: svn://svn.code.sf.net/p/segs/code2@172 a19be42f-918d-4297-8077-ab25d1e4f51f

--- Projects/CoX/docs/EventBased_Design/AuthServer.rb
+++ Projects/CoX/docs/EventBased_Design/AuthServer.rb
@@ -8,6 +8,11 @@ class AuthServer
 		end
 		post_event bytes_to_event(cl,ev.bytes)
 	end
+	def bytes_to_event(cl,bytes)
+		res = nil
+		
+		res.link = cl
+	end
 	def on_connect(ev)
 		cl=@clients.find_link_ip(ev.link.src_addr)
 		raise "Multiple connection attempts from the same addr/port" if(cl!=nil)
@@ -15,9 +20,12 @@ class AuthServer
 		client_link.state = :connected
 		client_link.post_event AuthorizationProtocolVersion.new(self,30206)
 	end
+	def on_disconnect(ev)
+		ev.link.state = :disconnected
+	end
 	# self posted events	
 	def on_auth_request(ev)
-		client_link = @clients.find_link_ip(ev.src_addr)
+		client_link = ev.link
 		if(client_link.state != :connected)
 			client_link.post_event AuthError.new(self,2,AuthServer::AUTH_UNKN_ERROR)
 			return
@@ -30,16 +38,44 @@ class AuthServer
 			client_link.post_event AuthError.new(self,2,auth_status)
 		end
 	end
+	
 	def on_server_list_request(ev)
-		client_link = @clients.find_link_ip(ev.src_addr)
+		client_link = ev.link
 		if(client_link.state != :logged_in)
 			client_link.post_event AuthError.new(self,2,AuthServer::AUTH_UNKN_ERROR)
-		end		
+			return
+		end
+		server_list_event = ServerList.new(self)
+		@known_game_servers.each {|gs|
+			server_list_event.add_server(gs.id,gs.address,gs.current_players,gs.max_players,gs.online)
+		}
+		client_link.state = :deciding_on_server # now client will select a server
+		client_link.post_event server_list_event		
 	end
-	
-	def on_expecting_client(ev) # event posted by Game server
+	def on_server_selected(ev)
+		# client selected a server
+		# we ask the selected server to give us a cookie
+		if(ev.link.state != :deciding_on_server)
+			ev.link.post_event AuthError.new(self,2,AuthServer::AUTH_UNKN_ERROR)
+			return
+		end
+		game_server = @known_game_servers[ev.serverId]
+		if(game_server == nil)
+			ev.link.post_event AuthError.new(self,2,AuthServer::AUTH_UNKN_ERROR)
+			return
+		end
+		game_server.post_event ExpectClient.new(self,ev.link.addr,ev.link.client_id,ev.link.access_level)
+		# as can be seen, we do not send anything to the client from here
+	end
+	# event posted by Game server
+	def on_expecting_client(ev) 
 		# now game server is expecting the client
-		# we can post the relevant packet to it
-		ev.client.post_event ServerResponse.new(self,ev.client,ev.db_cookie,ev.server_cookie)
+		# we can post the relevant cookies to it
+		client_link = @clients.find_by_id(ev.client_id)
+		if(client_link==nil)
+			# client might have disappeared before we receive response from GameServer
+			return
+		end
+		client_link.post_event ServerResponse.new(self,ev.client,ev.db_cookie,ev.server_cookie)
 	end
 end
