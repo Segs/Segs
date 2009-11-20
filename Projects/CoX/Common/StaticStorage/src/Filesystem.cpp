cef59bd73e2cfe3d3a822d3f9fc82c194db1a0dd
nemerle <nemerle@a19be42f-918d-4297-8077-ab25d1e4f51f>
2009-11-20 03:16:50 +0000

    Placeholder for static storage, this will be used to read various files, current goal is to read costume part names / colors.
    git-svn-id: svn://svn.code.sf.net/p/segs/code2@180 a19be42f-918d-4297-8077-ab25d1e4f51f

---
+++ Projects/CoX/Common/StaticStorage/src/Filesystem.cpp
@@ -0,0 +1,10 @@
+/*
+ * Super Entity Game Server Project
+ * http://segs.sf.net/
+ * Copyright (c) 2006 Super Entity Game Server Team (see Authors.txt)
+ * This software is licensed! (See License.txt for details)
+ *
+ * $Id: Client.cpp 253 2006-08-31 22:00:14Z malign $
+ */
+#include <ace/OS.h>
+#include "Filesystem.h"
