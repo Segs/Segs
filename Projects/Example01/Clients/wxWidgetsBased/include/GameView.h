fde956413f7e40e0bd6a9a5d36dd91e554436796
nemerle <nemerle@a19be42f-918d-4297-8077-ab25d1e4f51f>
2010-10-16 09:49:45 +0000

    Example01: remove wxWidgets+Ogre3d pseudo-client trash
    git-svn-id: svn://svn.code.sf.net/p/segs/code2@259 a19be42f-918d-4297-8077-ab25d1e4f51f

--- Projects/Example01/Clients/wxWidgetsBased/include/GameView.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "wxOgrePanel.h"
-class GameView : public wxOgrePanel
-{
-public:
-	GameView(wxWindow* parent,int win_id,wxPoint pos=wxDefaultPosition,wxSize sz = wxDefaultSize,int style = 0);
-	virtual ~GameView(void){};
-};
