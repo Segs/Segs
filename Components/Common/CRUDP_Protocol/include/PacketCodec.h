41a3391f87b10a35636c878c984a875ef6ace11b
nemerle <nemerle5@gmail.com>
2013-03-23 12:02:29 +0100

    naked_ace: Add a few missing includes to installed headers SEGS: modified the build system to use the built ACE, also added a missing <cstddef> include to PacketCodec.h

--- Components/Common/CRUDP_Protocol/include/PacketCodec.h
+++ Components/Common/CRUDP_Protocol/include/PacketCodec.h
@@ -14,6 +14,7 @@ the user.
 ************************************************************************/
 #pragma once
 #include <deque>
+#include <cstddef>
 #include <stdint.h>
 #ifdef USE_REAL_CODING
 #include <crypto++/cryptlib.h>
@@ -34,8 +35,8 @@ class PacketCodecNull
 {
 public:
     static uint32_t  Checksum(const uint8_t *buf,size_t size);
-    void Decrypt(uint8_t * /*arr*/,size_t /*size*/){};
-    void Encrypt(uint8_t * /*arr*/,size_t /*size*/){};
-    void StartEncryption(uint8_t * /*other_public_key*/){};
+    void Decrypt(uint8_t * /*arr*/,size_t /*size*/){}
+    void Encrypt(uint8_t * /*arr*/,size_t /*size*/){}
+    void StartEncryption(uint8_t * /*other_public_key*/){}
 
 };
