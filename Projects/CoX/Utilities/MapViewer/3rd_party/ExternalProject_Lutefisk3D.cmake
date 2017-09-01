faaa80e8bf0cad09b3997cb34575d620fe2da725
nemerle <nemerle5@gmail.com>
2017-09-01 14:27:25 +0200

    Add empty, lutefisk3d based, MapViewer.

---
+++ Projects/CoX/Utilities/MapViewer/3rd_party/ExternalProject_Lutefisk3D.cmake
@@ -0,0 +1,16 @@
+libname(lutefisk3d Lutefisk3D)
+ExternalProject_Add(
+   lutefisk3d_BUILD
+   GIT_REPOSITORY https://github.com/Lutefisk3D/lutefisk3d
+   GIT_SHALLOW 1
+   UPDATE_COMMAND ""
+   INSTALL_DIR ${ThirdParty_Install_Dir}
+   CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DCMAKE_STAGING_PREFIX:PATH=${ThirdParty_Install_Dir} -DCMAKE_INSTALL_PREFIX:PATH=${ThirdParty_Install_Dir}
+   CMAKE_GENERATOR "${CMAKE_GENERATOR}"
+   CMAKE_GENERATOR_TOOLSET ${CMAKE_GENERATOR_TOOLSET}
+   BUILD_BYPRODUCTS ${lutefisk3d_LIBRARY_SHARED} ${lutefisk3d_LIBRARY_IMP}
+)
+ADD_LIBRARY(lutefisk3d_IMP SHARED IMPORTED GLOBAL)
+add_dependencies(lutefisk3d_IMP lutefisk3d_BUILD)
+set_shared_lib_properties(lutefisk3d)
+#set_property(TARGET lutefisk3d_IMP APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS ACE_HAS_DLL=1)
---
+++ Projects/CoX/Utilities/MapViewer/src/main.cpp
@@ -0,0 +1,6 @@
+#include "Lutefisk3D/Engine/Engine.h"
+#include "Lutefisk3D/Engine/Application.h"
+
+int main(int argc,char **argv) {
+    return 0;
+}
