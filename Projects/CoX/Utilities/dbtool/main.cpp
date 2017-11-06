b1b806e8c5b903db2dfb78db495dcc49361b689f
broxen <paul@broxen.net>
2017-11-06 09:26:37 -0800

    Changes to MessageHandler, output, added some checks for files, changed to range loop

--- Projects/CoX/Utilities/dbtool/main.cpp
+++ Projects/CoX/Utilities/dbtool/main.cpp
@@ -30,27 +30,42 @@ void Pause(void)
     return;
 }
 
-void errorHandler(QtMsgType type, const char *msg)
+void errorHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
+    QByteArray localMsg = msg.toLocal8Bit();
+
+    #ifdef Q_OS_WIN
+    const char *colorTable[4] = {"","","",""};
+    const char * resetColor="";
+    #else
+    // assume all other platforms use VT color codes
+    const char * colorTable[4] = {"","\033[1;33m","\033[91m","\033[41m"};
+    const char * resetColor="\033[0m";
+    #endif
+
     switch (type) {
         case QtDebugMsg:
-            fprintf(stderr, "%s\n", msg);
+            fprintf(stderr, "%s\n", localMsg.constData());
+            break;
+        case QtInfoMsg:
+            fprintf(stderr, "%s\n", localMsg.constData());
             break;
         case QtWarningMsg:
-            fprintf(stderr, "\033[1;33mWarning\033[0m: %s\n", msg);
+            fprintf(stderr, "%sWARNING!%s %s\n", colorTable[1], resetColor, localMsg.constData());
             break;
         case QtCriticalMsg:
-            fprintf(stderr, "\033[31mCritical\033[0m: %s\n", msg);
+            fprintf(stderr, "%sCRITICAL!%s (%s:%u, %s)\n", colorTable[2], resetColor, localMsg.constData(), context.file, context.line, context.function);
             break;
         case QtFatalMsg:
-            fprintf(stderr, "\033[31mFatal\033[0m: %s\n", msg);
+            fprintf(stderr, "%sFATAL!%s (%s:%u, %s)\n", colorTable[3], resetColor, localMsg.constData(), context.file, context.line, context.function);
             abort();
     }
 }
 
 int main(int argc, char **argv)
 {
-    qInstallMsgHandler(errorHandler);
+    QLoggingCategory::setFilterRules("*.debug=true\nqt.*.debug=false");
+    qInstallMessageHandler(errorHandler);
     QCoreApplication app(argc,argv);
     QCoreApplication::setApplicationName("segs-dbtool");
     QCoreApplication::setApplicationVersion("0.2");
@@ -60,7 +75,7 @@ int main(int argc, char **argv)
     parser.addHelpOption();
     parser.addVersionOption();
 
-    qDebug() << parser.applicationDescription() << " v" << QCoreApplication::applicationVersion() << endl;
+    qInfo().noquote().nospace() << parser.applicationDescription() << " v" << QCoreApplication::applicationVersion() << endl;
     
     parser.addPositionalArgument("file", QCoreApplication::translate("main", "Database Script file to import."));
     
@@ -83,55 +98,70 @@ int main(int argc, char **argv)
         Pause();
         return 0;
     }
-    qDebug() << "Authserver Found!";
+    qInfo() << "Authserver Found!";
 
     // Check if database already exists
-    qInfo() << "Checking for existing databases...";
+    qInfo() << "Checking for existing databases OR -f command...";
     if((fileExists(segs) || fileExists(segs_game)) && !forced) {
         if(fileExists(segs))
-            qWarning() << "WARNING! Database " << segs << " already exists.";
+            qWarning() << "Database" << segs << "already exists.";
         if(fileExists(segs_game))
-            qWarning() << "WARNING! Database " << segs_game << " already exists.";
+            qWarning() << "Database" << segs_game << "already exists.";
         qDebug() << "Run dbtool with -f option to overwrite existing databases. THIS CANNOT BE UNDONE.";
         Pause();
         return 0;
     }
+
+    if(forced)
+        qWarning() << "Forced flag used '-f'. Existing databases may be overwritten.";
 
     qInfo() << "Creating database files...";
     QStringList db_files;
     if(nofile)
         db_files << "./default_dbs/segs" << "./default_dbs/segs_game";
     else
-        db_files << args;
+        db_files = args;
 
+    QSqlDatabase db = QSqlDatabase::addDatabase( "QSQLITE" );
     // Let's itterate over db_files and create database files
-    for (int i = 0; i < db_files.size(); ++i)
+    for (const QString &db_file : db_files)
     {
-        QFile db_template(db_file.at(i));
-        int last_slash = db_file.at(i).lastIndexOf('/',-1);
-        QFile db_path("./" + db_file.at(i).midRef(last_slash+1,-1)); // filename only: ./segs
+        if( ! QFileInfo(db_file).isReadable() )
+        {
+          qCritical() << db_file << "is not readable! Please check that the file is present and not corrupted.";
+          break;
+        }
+
+        QFile db_template(db_file);
+        int last_slash = db_file.lastIndexOf('/',-1);
+        QFile db_path("./" + db_file.midRef(last_slash+1,-1)); // filename only: ./segs
         
         // Otherwise, import contents of db_template into db_path
-        QSqlDatabase db = QSqlDatabase::addDatabase( "QSQLITE" );
-        db.setDatabaseName( db_path );
+        db.setDatabaseName(db_path.fileName());
 
         if( !db.open() )
         {
             qDebug() << db.lastError();
-            qFatal() << "Failed to connect to database. Please check error messages for details.";
-            Pause();
-            return 0;
+            qFatal("Failed to connect to database. Please check error messages for details.");
         }
-        qDebug() << "Created new database!";
 
         QSqlQuery qry;
         if(db_template.open(QFile::ReadOnly))
-            qry.exec(db_template.readAll());
+        {
+            qry.prepare(db_template.readAll());
+            if(!qry.exec())
+            {
+                qCritical() << qry.lastError();
+                break;
+            }
+        }
 
-        // Close our db
-        db.close();
-        qInfo() << "COMPLETED importing " << db_template << "!";
+        qInfo() << "COMPLETED importing" << db_template.fileName() << "to" << db_path.fileName();
     }
+
+    // Close and remove our db
+    db.close();
+    db.removeDatabase("QSQLITE");
 
     Pause();
     return 0;
