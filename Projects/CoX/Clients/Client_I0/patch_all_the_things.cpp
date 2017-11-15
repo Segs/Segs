5ccad3cfdeed870988343afd783b7569c0f0f4c1
nemerle <nemerle5@gmail.com>
2017-11-15 19:06:44 +0100

    Dummy patch dll to start things rolling.

---
+++ Projects/CoX/Clients/Client_I0/patch_all_the_things.cpp
@@ -0,0 +1,7 @@
+#include "patch_all_the_things.h"
+#include "utils/dll_patcher.h"
+// extern patch_named_module()
+void patch_all_the_things()
+{
+	//patch_named_module()
+}
---
+++ Projects/CoX/Clients/Client_I0/utils/dll_patcher.cpp
@@ -0,0 +1,28 @@
+#include "dll_patcher.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <windows.h>
+#include <cassert>
+void break_me() 
+{
+    assert(false);
+}
+
+extern HMODULE coh_instance;
+void patchit(const char *tgt,void *proc) 
+{
+    uint8_t *addr = (uint8_t *)GetProcAddress(coh_instance,tgt);
+    DWORD offset = ((DWORD)proc - (DWORD)addr - 5);
+    if(addr) 
+	{
+        *addr = 0xE9; // write JMP
+        *(DWORD *)(addr+1) = (DWORD)offset; // write target offset
+        printf("Patched %s\n",tgt);
+    } 
+	else 
+	{
+        printf("Couldn't patch %s\n",tgt);
+        abort();
+    }
+}
