4335ae26b942455ab15a00cecec50db2b094748b
erpmario <34783705+erpmario@users.noreply.github.com>
2018-01-02 11:45:12 -0600

    Clean up AuthExerciser
    Change `SERVER_HOST` and `serverHost` types from `const char*` to `QString` to avoid losing values.
    
    Add more helpful comments along with debug messages for successful disconnections.

--- Projects/CoX/Utilities/AuthExerciser/main.cpp
+++ Projects/CoX/Utilities/AuthExerciser/main.cpp
@@ -9,7 +9,7 @@
 #include <QtCore/QDir>
 
 static uint16_t SERVER_PORT = ACE_DEFAULT_SERVER_PORT;
-static const char* const SERVER_HOST = ACE_DEFAULT_SERVER_HOST;
+static QString const SERVER_HOST = ACE_DEFAULT_SERVER_HOST;
 static const int MAX_ITERATIONS = 4;
 
 int main(int argc, char** argv)
@@ -31,24 +31,24 @@ int main(int argc, char** argv)
 
     parser.process(app);
 
-    const QStringList args = parser.positionalArguments();
-    const char* arg0 = args[0].toLatin1().data();
-    const char* serverHost = args.length() >= 1 ? arg0 : SERVER_HOST; // If args.length() > 1, serverHost = arg0. Else, SERVER_HOST.
-    uint16_t serverPort = args.length() >= 2 ? ACE_OS::atoi(qPrintable(args[1])) : SERVER_PORT; // If args.length() >= 2, serverPort = qPrintable(args[1]). Else, SERVER_PORT.
-    int maxIterations = args.length() >= 3 ? ACE_OS::atoi(qPrintable(args[2])) : MAX_ITERATIONS; // Same format as above.
+    const QStringList args = parser.positionalArguments(); // Grab the arguments from the parser.
+    QString serverHost = args.length() >= 1 ? args[0] : SERVER_HOST; // Place them in their own variables, or use the default values if they aren't given.
+    uint16_t serverPort = args.length() >= 2 ? ACE_OS::atoi(qPrintable(args[1])) : SERVER_PORT;
+    int maxIterations = args.length() >= 3 ? ACE_OS::atoi(qPrintable(args[2])) : MAX_ITERATIONS;
 
     ACE_SOCK_Stream server;
     ACE_SOCK_Connector connector;
-    ACE_INET_Addr addr(serverPort, serverHost);
+    ACE_INET_Addr addr(serverPort, qPrintable(serverHost));
 
-    qInfo().noquote().nospace() << "IP and Port entered: " << arg0 << ":" << serverPort;
+    qInfo().noquote().nospace() << "IP and Port entered: " << serverHost << ":" << serverPort;
     qInfo().noquote().nospace() << "Attempting to connect to authserver...";
 
-    if(connector.connect(server, addr) == -1)
+    if(connector.connect(server, addr) == -1) // Connection fails.
         ACE_ERROR_RETURN((LM_ERROR, "%p\n", "open"), -1);
-    else
+    else // Connection succeeds.
         qInfo().noquote().nospace() << "Success!";
 
+    // Attempt to communicate with authserver.
     for(int i = 0; i < maxIterations; i++)
     {
         char buf[BUFSIZ];
@@ -61,8 +61,10 @@ int main(int argc, char** argv)
             ACE_OS::sleep(1);
     }
 
-    if(server.close() == -1)
+    if(server.close() == -1) // Close attempt fails.
         ACE_ERROR_RETURN((LM_ERROR, "%p\n", "close"), -1);
+    else // Close attempt succeeds.
+        qInfo().noquote().nospace() << "Disconnecting gracefully...";
 
     return 0;
 }
