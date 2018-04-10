dc5c6bba146021f1d714648c67fbed1b79ca79ae
nemerle <nemerle5@gmail.com>
2018-04-10 14:24:55 +0200

    [skip ci] Introduce InternalEvent class that stores the session_token to properly 'mark' events relating to the same session.

--- Projects/CoX/Common/Servers/InternalEvents.h
+++ Projects/CoX/Common/Servers/InternalEvents.h
@@ -15,14 +15,23 @@ public:
     EVENT_DECL(evClientConnectionResponse,3)
     END_EVENTS(4)
 };
+class InternalEvent : public SEGSEvent
+{
+    uint64_t            m_session_token = 0;
+public:
+    using SEGSEvent::SEGSEvent; // forward all constructors to parent class.
 
+    void                session_token(uint64_t token) { m_session_token = token; }
+    uint64_t            session_token() const { return m_session_token; }
+
+};
 // This tells the server that it should expect a new client connection from given address
-class ExpectClientRequest : public SEGSEvent
+class ExpectClientRequest : public InternalEvent
 {
     ExpectClientRequest() = delete;
 public:
     ExpectClientRequest(EventProcessor *evsrc,uint64_t client_id,uint8_t access_level,const ACE_INET_Addr &from) :
-                SEGSEvent(Internal_EventTypes::evExpectClientRequest,evsrc),
+                InternalEvent(Internal_EventTypes::evExpectClientRequest,evsrc),
                 m_client_id(client_id),
                 m_access_level(access_level),
                 m_from_addr(from)
@@ -55,11 +64,11 @@ public:
 };
 // This event informs the server that given client is now expected on another server
 // and passes that servers connection point, and connection cookie
-class ClientExpected : public SEGSEvent
+class ClientExpected : public InternalEvent
 {
 public:
         ClientExpected(EventProcessor *evsrc,uint64_t cid,uint32_t c,const ACE_INET_Addr &tgt) :
-                        SEGSEvent(Internal_EventTypes::evClientExpected,evsrc),
+                        InternalEvent(Internal_EventTypes::evClientExpected,evsrc),
                         client_id(cid),
                         cookie(c),
                         m_connection_addr(tgt)
@@ -71,20 +80,20 @@ public:
 
 // Called synchronously this query is used to retrieve client's connection status.
 // Maybe the map/game servers should just post ClientConnection updates to AuthServer ?
-class ClientConnectionQuery : public SEGSEvent
+class ClientConnectionQuery : public InternalEvent
 {
 public:
-    ClientConnectionQuery(EventProcessor *evsrc,uint64_t id) : SEGSEvent(Internal_EventTypes::evClientConnectionQuery,evsrc),m_id(id)
+    ClientConnectionQuery(EventProcessor *evsrc,uint64_t id) : InternalEvent(Internal_EventTypes::evClientConnectionQuery,evsrc),m_id(id)
     {
 
     }
     uint64_t m_id;
 };
 
-class ClientConnectionResponse : public SEGSEvent
+class ClientConnectionResponse : public InternalEvent
 {
 public:
-    ClientConnectionResponse(EventProcessor *evsrc,const ACE_Time_Value &lc) : SEGSEvent(Internal_EventTypes::evClientConnectionResponse,evsrc),last_comm(lc)
+    ClientConnectionResponse(EventProcessor *evsrc,const ACE_Time_Value &lc) : InternalEvent(Internal_EventTypes::evClientConnectionResponse,evsrc),last_comm(lc)
     {
     }
     ACE_Time_Value last_comm;
