6c43c056d8b82fe6760bf12ea134bc78da135669
nemerle <nemerle5@gmail.com>
2018-04-12 13:18:44 +0200

    Starting another merge-phase for the server separation work
    A few unrelated fixes/changes first
    
    * Remove disregard_seq argument from RecvPacket, simplify it at the same
    time
    * started to mark ACE includes as SYSTEM to prevent more thorough
    warning modes spewing loads of ACE related warnings.

--- Components/EventProcessor.cpp
+++ Components/EventProcessor.cpp
@@ -13,7 +13,7 @@ int EventProcessor::open( void *args /* = 0 */ )
 int EventProcessor::handle_timeout( const ACE_Time_Value &current_time, const void *act /* = 0 */ )
 {
     const SEGSTimer *timer_object = static_cast<const SEGSTimer *>(act);
-    assert(timer_object!=0);
+    assert(timer_object!=nullptr);
     // if target is known
     if(timer_object->target())
     {
