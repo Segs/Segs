f43081618c888faa397c860c3ff8b6e9fceeaff6
nemerle <nemerle5@gmail.com>
2018-06-07 15:36:09 +0200

    Forgot to update version string

--- include/version.h
+++ include/version.h
@@ -6,7 +6,7 @@
 *
 */
 
-#define VersionString "segs v0.4.3 (The Real Beginning)";
+#define VersionString "segs v0.5.0 (The Unsilencer)";
 #define CopyrightString "Super Entity Game Server\nhttp://github.com/Segs/\nCopyright (c) 2006-2018 Super Entity Game Server Team (see Authors.txt)\nThis software is licensed! (See License.txt for details)\n";
 //const char *AdminVersionString="Undefined";
 //const char *AuthVersionString="Undefined";
