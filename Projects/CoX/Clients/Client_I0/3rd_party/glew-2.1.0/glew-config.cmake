ad2c18b94c06cd98a617528dd68ed468e805334d
nemerle <nemerle5@gmail.com>
2018-06-10 19:05:37 +0200

    ClientPatch: update glew to 2.1.0

--- Projects/CoX/Clients/Client_I0/3rd_party/glew-2.1.0/glew-config.cmake
+++ Projects/CoX/Clients/Client_I0/3rd_party/glew-2.1.0/glew-config.cmake
@@ -1,23 +1,23 @@
 # This config-module creates the following import libraries:
 #
-# - GLEW::glew and GLEW::glewmx shared libs
-# - GLEW::glew_s and GLEW::glewmx_s static libs
+# - GLEW::glew shared lib
+# - GLEW::glew_s static lib
 #
-# Additionally GLEW::GLEW and GLEW::GLEWMX will be created as an
+# Additionally GLEW::GLEW will be created as an
 # copy of either the shared (default) or the static libs.
 #
 # Dependending on the setting of BUILD_SHARED_LIBS at GLEW build time
 # either the static or shared versions may not be available.
 #
 # Set GLEW_USE_STATIC_LIBS to OFF or ON to force using the shared
-# or static libs for GLEW::GLEW and GLEW::GLEWMX
+# or static lib for GLEW::GLEW 
 #
 
 include(${CMAKE_CURRENT_LIST_DIR}/glew-targets.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/CopyImportedTargetProperties.cmake)
 
-# decide which import library (glew/glew_s and glewmx/glewmx_s)
-# needs to be copied to GLEW::GLEW and GLEW::GLEWMX
+# decide which import library (glew/glew_s)
+# needs to be copied to GLEW::GLEW
 set(_glew_target_postfix "")
 set(_glew_target_type SHARED)
 if(DEFINED GLEW_USE_STATIC_LIBS)
@@ -37,10 +37,24 @@ endif()
 
 # CMake doesn't allow creating ALIAS lib for an IMPORTED lib
 # so create imported ones and copy the properties
-foreach(_glew_target glew glewmx)
+foreach(_glew_target glew)
   set(_glew_src_target "GLEW::${_glew_target}${_glew_target_postfix}")
   string(TOUPPER "GLEW::${_glew_target}" _glew_dest_target)
-  add_library(${_glew_dest_target} ${_glew_target_type} IMPORTED)
-  # message(STATUS "add_library(${_glew_dest_target} ${_glew_target_type} IMPORTED)")
-  copy_imported_target_properties(${_glew_src_target} ${_glew_dest_target})
+  if(TARGET ${_glew_dest_target})
+    get_target_property(_glew_previous_src_target ${_glew_dest_target}
+      _GLEW_SRC_TARGET)
+    if(NOT _glew_previous_src_target STREQUAL _glew_src_target)
+      message(FATAL_ERROR "find_package(GLEW) was called the second time with "
+        "different GLEW_USE_STATIC_LIBS setting. Previously, "
+        "`glew-config.cmake` created ${_glew_dest_target} as a copy of "
+        "${_glew_previous_src_target}. Now it attempted to copy it from "
+        "${_glew_src_target}. ")
+    endif()
+  else()
+    add_library(${_glew_dest_target} ${_glew_target_type} IMPORTED)
+    # message(STATUS "add_library(${_glew_dest_target} ${_glew_target_type} IMPORTED)")
+    copy_imported_target_properties(${_glew_src_target} ${_glew_dest_target})
+    set_target_properties(${_glew_dest_target} PROPERTIES
+      _GLEW_SRC_TARGET ${_glew_src_target})
+  endif()
 endforeach()
