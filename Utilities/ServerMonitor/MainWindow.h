7a9f84b502a36f2da363ecae7092c185450fa814
Lucas Eriksson <dracc@dracc.se>
2018-06-14 16:49:25 +0200

    [skip ci] License fix Update license information and refer to the correct AUTHORS file.

--- Utilities/ServerMonitor/MainWindow.h
+++ Utilities/ServerMonitor/MainWindow.h
@@ -1,8 +1,8 @@
 /*
  * SEGS - Super Entity Game Server
  * http://www.segs.io/
- * Copyright (c) 2006 - 2018 SEGS Team (see Authors.txt)
- * This software is licensed! (See License.txt for details)
+ * Copyright (c) 2006 - 2018 SEGS Team (see AUTHORS.md)
+ * This software is licensed under the terms of the 3-clause BSD License. See LICENSE.md for details.
  */
 
 #ifndef MAINWINDOW_H
