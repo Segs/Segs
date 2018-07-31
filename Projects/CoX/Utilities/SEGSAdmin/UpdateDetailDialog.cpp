b0e229eb8b2daf97ec4dd65492254c024324a1a5
miles200 <35471210+miles200@users.noreply.github.com>
2018-07-31 22:47:36 +0100

    - Add ability to check for updates on start up utilising the github API - UI tweaks, icons added and new splash screen (temp in place atm)

---
+++ Projects/CoX/Utilities/SEGSAdmin/UpdateDetailDialog.cpp
@@ -0,0 +1,50 @@
+/*
+ * SEGS - Super Entity Game Server
+ * http://www.segs.io/
+ * Copyright (c) 2006 - 2018 SEGS Team (see AUTHORS.md)
+ * This software is licensed under the terms of the 3-clause BSD License. See LICENSE.md for details.
+ */
+
+/*!
+ * @addtogroup SEGSAdmin Projects/CoX/Utilities/SEGSAdmin
+ * @{
+ */
+
+#include "UpdateDetailDialog.h"
+#include "ui_UpdateDetailDialog.h"
+#include "Globals.h"
+#include <QUrl>
+#include <QDesktopServices>
+
+UpdateDetailDialog::UpdateDetailDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::UpdateDetailDialog)
+{
+    ui->setupUi(this);
+    QFont dejavu_font;
+    dejavu_font.setFamily("DejaVu Sans Condensed");
+    dejavu_font.setPointSize(12);
+    ui->update_notes->setFont(dejavu_font);
+    connect(ui->download_button,&QPushButton::clicked,this,&UpdateDetailDialog::open_download_url);
+
+}
+
+UpdateDetailDialog::~UpdateDetailDialog()
+{
+    delete ui;
+}
+
+void UpdateDetailDialog::show_update()
+{
+    ui->version_label->setText(g_segs_release_info[0].tag_name);
+    ui->version_name->setText(g_segs_release_info[0].release_name);
+    ui->update_notes->setText(g_segs_release_info[0].release_notes);
+    show();
+}
+
+void UpdateDetailDialog::open_download_url()
+{
+    QDesktopServices::openUrl((QUrl(g_segs_release_info[0].github_url)));
+}
+
+//!@}
