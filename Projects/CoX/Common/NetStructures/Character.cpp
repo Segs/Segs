6f4ec945e959d44e9327629096851e3bd2b74139
eos428 <night39gooner@gmail.com>
2018-09-21 20:48:28 +0700

    Some changes to Character.cpp as per broxen's comments

--- Projects/CoX/Common/NetStructures/Character.cpp
+++ Projects/CoX/Common/NetStructures/Character.cpp
@@ -103,6 +103,9 @@ void Character::finalizeLevel()
     GameDataStore *data = getMapServerData();
     uint32_t max_xp = data->expMaxLevel();
 
+    if (m_char_data.m_level == 4294967295 || m_char_data.m_level >= 50)
+        m_char_data.m_level = 0;// /levelup can set this negative, need to bring it up
+
     m_char_data.m_combat_level = m_char_data.m_level + 1; // m_combat_level is display level?
 
     if(m_char_data.m_combat_level >= max_xp)
@@ -111,9 +114,9 @@ void Character::finalizeLevel()
     if(m_char_data.m_experience_points < data->expForLevel(m_char_data.m_combat_level))
         m_char_data.m_experience_points = data->expForLevel(m_char_data.m_combat_level);
 
-    m_char_data.m_max_insp_rows = data->countForLevel(m_char_data.m_combat_level, data->m_pi_schedule.m_InspirationRow);
-    m_char_data.m_max_insp_cols = data->countForLevel(m_char_data.m_combat_level, data->m_pi_schedule.m_InspirationCol);
-    m_char_data.m_max_enhance_slots = data->countForLevel(m_char_data.m_combat_level, data->m_pi_schedule.m_BoostSlot);
+    m_char_data.m_max_insp_rows = data->countForLevel(m_char_data.m_level, data->m_pi_schedule.m_InspirationRow);
+    m_char_data.m_max_insp_cols = data->countForLevel(m_char_data.m_level, data->m_pi_schedule.m_InspirationCol);
+    m_char_data.m_max_enhance_slots = data->countForLevel(m_char_data.m_level, data->m_pi_schedule.m_BoostSlot);
 
     // TODO: client will only accept 5col x 4row of insps MAX, see Issue #524
     assert(m_char_data.m_max_insp_cols <= 5 || m_char_data.m_max_insp_rows <= 4);
