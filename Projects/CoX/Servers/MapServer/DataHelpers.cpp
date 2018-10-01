0434cdab997cbc5ce0060292cdfc727bcee87088
cyanpill <cyanpill@gmail.com>
2018-10-01 15:17:06 -0400

    small fix

--- Projects/CoX/Servers/MapServer/DataHelpers.cpp
+++ Projects/CoX/Servers/MapServer/DataHelpers.cpp
@@ -740,7 +740,9 @@ void usePower(Entity &ent, uint32_t pset_idx, uint32_t pow_idx, uint32_t tgt_idx
     messageOutput(MessageChannel::COMBAT, console_msg, *target_ent);
     }
 }
-void findTeamMember(Entity &tgt){
-    sendTeamLooking(&tgt);}
+void findTeamMember(Entity &tgt)
+{
+    sendTeamLooking(&tgt);
+}
 //! @}
 
