682ee4aa95611bab7cb67c68a727cd02d9d22b82
ZopharShinta <43687491+ZopharShinta@users.noreply.github.com>
2018-12-09 00:13:52 -0600

    Added commands to scripting engine / LuaBot

--- Projects/CoX/Servers/MapServer/ScriptingEngine.cpp
+++ Projects/CoX/Servers/MapServer/ScriptingEngine.cpp
@@ -349,6 +349,18 @@ void ScriptingEngine::registerTypes()
         MapClientSession *cl = m_private->m_lua["client"];
         sendInfoMessage(static_cast<MessageChannel>(channel), QString::fromUtf8(message), *cl);
     };
+    m_private->m_lua["MapClientSession"]["ConsoleOutput"] = [this](const char* message)
+    {
+        MapClientSession *cl = m_private->m_lua["client"];
+        QString msg = QString::fromUtf8(message);
+        sendConsoleOutput(*cl, msg);
+    };
+    m_private->m_lua["MapClientSession"]["ConsolePrint"] = [this](const char* message)
+    {
+        MapClientSession *cl = m_private->m_lua["client"];
+        QString msg = QString::fromUtf8(message);
+        sendConsolePrint(*cl, msg);
+    };
 
     m_private->m_lua.new_usertype<Entity>( "Entity",
         "new",    sol::no_constructor, // not constructible from the script side.
