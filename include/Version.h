591ca4d6f0e69b7b2f186f02720389a8a7ff02e5
broxen <paul@broxen.net>
2019-03-17 03:26:30 -0700

    [skip ci] formatting changes

--- include/Version.h
+++ include/Version.h
@@ -6,10 +6,20 @@
  */
 
 #pragma once
+
+#ifndef SEGS_VERSION
+#define SEGS_VERSION "0.6.1" // set in cmake, but defined here if not available
+#endif
+#ifndef SEGS_BRANCH
+#define SEGS_BRANCH "unknown"
+#endif
+#ifndef SEGS_DESCRIPTION
+#define SEGS_DESCRIPTION "unknown"
+#endif
+
 #define ProjectName "SEGS"
-#define VersionNumber SEGS_VERSION // from cmake and git revision
 #define VersionName "Outbreak"
-#define VersionString ProjectName " v" VersionNumber " (" VersionName ") [" SEGS_BRANCH ": " SEGS_REVISION "]"
+#define VersionString ProjectName " v" SEGS_VERSION "-" SEGS_BRANCH " (" VersionName ") [" SEGS_DESCRIPTION "]"
 #define CopyrightString "Super Entity Game Server\nhttp://github.com/Segs/\nCopyright (c) 2006-2019 Super Entity Game Server Team (see AUTHORS.md)\nThis software is licensed under the terms of the 3-clause BSD License. See LICENSE.md for details.\n";
 
 static constexpr int s_config_version = 1;
@@ -21,7 +31,7 @@ class VersionInfo
 {
 public:
     static const char *getAuthVersion(void) { return VersionString; }
-    static const char *getAuthVersionNumber(void) { return VersionNumber;}
+    static const char *getAuthVersionNumber(void) { return SEGS_VERSION; }
     static const char *getVersionName(void) { return VersionName; }
     static const char *getCopyright(void){ return CopyrightString; }
     static constexpr int getConfigVersion(void) { return s_config_version; }
@@ -31,7 +41,9 @@ public:
 
 #undef ProjectName
 #undef VersionName
-#undef VersionNumber
 #undef VersionString
 #undef CopyrightString
+#undef SEGS_VERSION
+#undef SEGS_BRANCH
+#undef SEGS_DESCRIPTION
 
