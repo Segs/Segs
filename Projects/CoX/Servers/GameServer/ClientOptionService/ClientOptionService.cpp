ea8ed3861f77ca3650281e92a965dfd7d11ef0b1
eos428 <zkt1088@gmail.com>
2020-05-08 21:07:44 +0700

    refactor: use std::move(casted_ev) in lambda expression instead of directly using casted_ev

--- Projects/CoX/Servers/GameServer/ClientOptionService/ClientOptionService.cpp
+++ Projects/CoX/Servers/GameServer/ClientOptionService/ClientOptionService.cpp
@@ -18,14 +18,14 @@ UPtrServiceToEntityData ClientOptionService::on_set_keybind(Event *ev)
 {
     SetKeybind* casted_ev = static_cast<SetKeybind *>(ev);
 
-    auto func = [casted_ev](Entity* ent) -> void
+    auto func = [content_data = std::move(casted_ev)](Entity* ent) -> void
     {
-        KeyName key = static_cast<KeyName>(casted_ev->key);
-        ModKeys mod = static_cast<ModKeys>(casted_ev->mods);
+        KeyName key = static_cast<KeyName>(content_data->key);
+        ModKeys mod = static_cast<ModKeys>(content_data->mods);
 
-        ent->m_player->m_keybinds.setKeybind(casted_ev->profile, key, mod, casted_ev->command, casted_ev->is_secondary);
-        qCDebug(logKeybinds) << "Setting keybind: " << casted_ev->profile << QString::number(casted_ev->key)
-                              << QString::number(casted_ev->mods) << casted_ev->command << casted_ev->is_secondary;
+        ent->m_player->m_keybinds.setKeybind(content_data->profile, key, mod, content_data->command, content_data->is_secondary);
+        qCDebug(logKeybinds) << "Setting keybind: " << content_data->profile << QString::number(content_data->key)
+                              << QString::number(content_data->mods) << content_data->command << content_data->is_secondary;
     };
 
     return std::make_unique<ServiceToEntityData>(get_session_token(ev), func);
@@ -35,10 +35,10 @@ UPtrServiceToEntityData ClientOptionService::on_save_client_options(Event *ev)
 {
     SaveClientOptions* casted_ev = static_cast<SaveClientOptions *>(ev);
 
-    auto func = [casted_ev](Entity* ent) -> void
+    auto func = [content_data = std::move(casted_ev)](Entity* ent) -> void
     {
         markEntityForDbStore(ent,DbStoreFlags::PlayerData);
-        ent->m_player->m_options = casted_ev->data;
+        ent->m_player->m_options = content_data->data;
     };
 
     return std::make_unique<ServiceToEntityData>(get_session_token(ev), func);
@@ -48,10 +48,10 @@ UPtrServiceToEntityData ClientOptionService::on_remove_keybind(Event *ev)
 {
     RemoveKeybind* casted_ev = static_cast<RemoveKeybind *>(ev);
 
-    auto func = [casted_ev](Entity* ent) -> void
+    auto func = [content_data = std::move(casted_ev)](Entity* ent) -> void
     {
-        ent->m_player->m_keybinds.removeKeybind(casted_ev->profile,(KeyName &)casted_ev->key,(ModKeys &)casted_ev->mods);
-        qCDebug(logKeybinds) << "Clearing Keybind: " << casted_ev->profile << QString::number(casted_ev->key) << QString::number(casted_ev->mods);
+        ent->m_player->m_keybinds.removeKeybind(content_data->profile,(KeyName &)content_data->key,(ModKeys &)content_data->mods);
+        qCDebug(logKeybinds) << "Clearing Keybind: " << content_data->profile << QString::number(content_data->key) << QString::number(content_data->mods);
     };
 
     return std::make_unique<ServiceToEntityData>(get_session_token(ev), func);
@@ -61,10 +61,10 @@ UPtrServiceToEntityData ClientOptionService::on_select_keybind_profile(Event *ev
 {
     SelectKeybindProfile* casted_ev = static_cast<SelectKeybindProfile *>(ev);
 
-    auto func = [casted_ev](Entity* ent) -> void
+    auto func = [content_data = std::move(casted_ev)](Entity* ent) -> void
     {
-        ent->m_player->m_keybinds.setKeybindProfile(casted_ev->profile);
-        qCDebug(logKeybinds) << "Saving currently selected Keybind Profile. Profile name: " << casted_ev->profile;
+        ent->m_player->m_keybinds.setKeybindProfile(content_data->profile);
+        qCDebug(logKeybinds) << "Saving currently selected Keybind Profile. Profile name: " << content_data->profile;
     };
 
     return std::make_unique<ServiceToEntityData>(get_session_token(ev), func);
@@ -74,7 +74,7 @@ UPtrServiceToEntityData ClientOptionService::on_reset_keybinds(Event *ev)
 {
     ResetKeybinds* casted_ev = static_cast<ResetKeybinds *>(ev);
 
-    auto func = [casted_ev](Entity* ent) -> void
+    auto func = [content_data = std::move(casted_ev)](Entity* ent) -> void
     {
         const GameDataStore &data(getGameData());
         const Parse_AllKeyProfiles &default_profiles(data.m_keybind_profiles);
@@ -90,10 +90,10 @@ UPtrServiceToEntityData ClientOptionService::on_switch_viewpoint(Event *ev)
 {
     SwitchViewPoint* casted_ev = static_cast<SwitchViewPoint *>(ev);
 
-    auto func = [casted_ev](Entity* ent) -> void
+    auto func = [content_data = std::move(casted_ev)](Entity* ent) -> void
     {
-        ent->m_player->m_options.m_first_person_view = casted_ev->new_viewpoint_is_firstperson;
-        qCDebug(logKeybinds) << "Saving viewpoint mode to ClientOptions" << casted_ev->new_viewpoint_is_firstperson;
+        ent->m_player->m_options.m_first_person_view = content_data->new_viewpoint_is_firstperson;
+        qCDebug(logKeybinds) << "Saving viewpoint mode to ClientOptions" << content_data->new_viewpoint_is_firstperson;
     };
 
     return std::make_unique<ServiceToEntityData>(get_session_token(ev), func);
